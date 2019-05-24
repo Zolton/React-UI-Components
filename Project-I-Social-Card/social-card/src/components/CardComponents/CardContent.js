fec36ff52a2a8bd46f7458c8907ea1abedef05f7
Zolton <gmzesq@gmail.com>
2019-05-24 19:11:11 -0400

    Fixed some things with Project 1 MVP

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -3,7 +3,7 @@ import './Card.css';
 
 const CardContent = props => {
     return (
-    <a href="https://www.reactjs.org">
+    <a className = "cardContent1" href="https://www.reactjs.org">
         <h1>Get started with React</h1>
         <h3>React makes it painless to create interactive UIs. Design simple views for each state in your application</h3>
 
