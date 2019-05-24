f1ff8ee1665c4ac520d225ef4aa20b571c172a5f
Zolton <gmzesq@gmail.com>
2019-05-24 18:27:11 -0400

    Finished Project 1 MVP

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = props => {
+    return (
+    <a href="https://www.reactjs.org">
+        <h1>Get started with React</h1>
+        <h3>React makes it painless to create interactive UIs. Design simple views for each state in your application</h3>
+
+    </a>
+    );
+}
+
+export default CardContent;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = props => {
+     return <img className="lambdaLogo" src="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png" alt="img" />;
+     //Backgrounds = "https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png"
+     //return <img src={ Backgrounds } />
+}
+
+export default ImageThumbnail;
+
